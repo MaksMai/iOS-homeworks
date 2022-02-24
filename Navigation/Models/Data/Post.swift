1afae6608cf752c792b918c09f69d9dcccb5a2eb
Maksim Maiorov <mvmayorov@icloud.com>
2022-02-24 11:01:26 +0300

    Домашние задание (часть №1)

---
+++ Navigation/Models/Data/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Maksim Maiorov on 09.02.2022.
+//
+
+import UIKit
+
+struct Post { // Создаем структуру Post со свойством title: String.
+    var title: String
+}
